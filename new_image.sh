c80c640acb50e58dcaf5482b88b9a65c82fabba2
Wonsup Yoon <pusnow@me.com>
2019-12-26 19:38:05 +0900

    load parameter config

--- new_image.sh
+++ new_image.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-source config/user
+if [ "$#" -eq 0 ]; then
+    echo "Loading config/user"
+    source config/user
+elif [ "$#" -eq 1 ]; then
+    echo "Loading $1"
+    source $1 || echo "Not found: $1" && exit 1
+else
+    echo "Illegal number of parameters."
+    exit 1
+fi
 qemu-img create -f qcow2 $DISK_PATH $DISK_SIZE
