3f26cbac79a349a34d2b864bb898f5dec1ede622
Wonsup Yoon <pusnow@kaist.ac.kr>
2019-12-26 12:48:25 +0900

    use virtio devices

--- run.sh
+++ run.sh
@@ -14,7 +14,10 @@ esac
 
 source config/user
 
-QEMU_ARGS="-machine q35,accel=$ACCEL -smp $CORES -accel $ACCEL -m $MEMORY -monitor stdio -name \"$NAME\" -usb -device usb-tablet"
+QEMU_ARGS="-machine q35,accel=$ACCEL -smp $CORES -accel $ACCEL -m $MEMORY -monitor stdio -name \"$NAME\""
+QEMU_ARGS="$QEMU_ARGS -device virtio-tablet-pci"
+QEMU_ARGS="$QEMU_ARGS -device virtio-balloon-pci"
+QEMU_ARGS="$QEMU_ARGS -device virtio-vga"
 QEMU_ARGS="$QEMU_ARGS -drive file=$DISK_PATH,if=virtio,cache=writeback,cache.direct=on$DISK_OPT,format=qcow2"
 
 case "${NETWORKING}" in
